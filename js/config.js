68b996886cfef1b8f01381f222f68295d64e11b0
Brandon Lehmann <brandonlehmann@gmail.com>
2018-11-28 13:48:03 -0500

    Deprecating nodes

--- js/config.js
+++ js/config.js
@@ -1,12 +1,8 @@
 const apiInterface = 'https://api.turtlenode.io/'
 const nodes = [
   { name: 'US-East-1', hostname: 'node-1.nyc.turtlenode.io', port: 11898, region: 'US East' },
-  { name: 'US-East-2', hostname: 'node-2.nyc.turtlenode.io', port: 11898, region: 'US East' },
   { name: 'US-West-1', hostname: 'node-1.sfo.turtlenode.io', port: 11898, region: 'US West' },
-  { name: 'US-West-2', hostname: 'node-2.sfo.turtlenode.io', port: 11898, region: 'US West' },
   { name: 'EU-AMS-1', hostname: 'node-1.ams.turtlenode.io', port: 11898, region: 'Europe' },
-  { name: 'EU-AMS-2', hostname: 'node-2.ams.turtlenode.io', port: 11898, region: 'Europe' },
   { name: 'AS-SIN-1', hostname: 'node-1.sin.turtlenode.io', port: 11898, region: 'Asia' },
-  { name: 'AS-SIN-2', hostname: 'node-2.sin.turtlenode.io', port: 11898, region: 'Asia' },
   { name: 'SEED', hostname: 'seed.turtlenode.io', port: 11898, region: 'US East' }
 ]
