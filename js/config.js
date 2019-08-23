7ab72121a3983336ec2cfcd4ef0ed215875c1480
keda666 <53265423+keda666@users.noreply.github.com>
2019-08-24 00:28:05 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 const apiInterface = 'https://api.turtlenode.io/'
 const nodes = [
-  { name: 'US-East-1', hostname: 'node-1.nyc.turtlenode.io', port: 11898, region: 'US East' },
-  { name: 'SEED', hostname: 'seed.turtlenode.io', port: 11898, region: 'US East' }
+  { name: 'AMS1', hostname: 'ams1.turtlenode.org', port: 11898, region: 'EU Amsterdam 1' },
+  { name: 'PAR1', hostname: 'par1.turtlenode.org', port: 11898, region: 'EU Paris 1' }
 ]
